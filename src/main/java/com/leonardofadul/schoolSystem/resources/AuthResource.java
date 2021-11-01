12efe6d51198f8ddb56d632e859a941e6c2c281d
Leonardo Fadul <lsffadul@gmail.com>
2021-11-01 11:07:54 -0300

    Exposing authorization header

--- src/main/java/com/leonardofadul/schoolSystem/resources/AuthResource.java
+++ src/main/java/com/leonardofadul/schoolSystem/resources/AuthResource.java
@@ -20,20 +20,12 @@ public class AuthResource {
     @Autowired
     private JWTUtil jwtUtil;
 
-//    @Autowired
-//    private AuthService authService;
-
     @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
     public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
         UserSS user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("access-control-expose-headers", "Authorization");
         return ResponseEntity.noContent().build();
     }
-
-//    @RequestMapping(value="/forgot", method= RequestMethod.POST)
-//    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
-//        authService.sendNewPassword(objDto.getEmail());
-//        return ResponseEntity.noContent().build();
-//    }
 }
